08a520c59799a7c8ceb5534cfcdbd10d1e948557
Hagar Shilo <strayblues@gmail.com>
2016-12-12 19:42:53 +0200

    Fix the saving in ffx bug

--- main.js
+++ main.js
@@ -48,12 +48,14 @@ function init() {
 
 }
 
-function downloadURI(uri, name)
-{
-    var link = document.createElement("a");
-    link.download = name;
-    link.href = uri;
-    link.click();
+function downloadURI(uri, name) {
+  var link = document.createElement("a");
+  link.download = name;
+  link.href = uri;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  delete link;
 }
 
 function drawDot() {
