65b34d9ba7e4caaf8047812c48ab281bbfb60371
Hagar Shilo <strayblues@gmail.com>
2017-04-23 00:01:02 +0300

    Change address to mobile

--- docs/main.js
+++ docs/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   setTimeout(function () {
-   window.location.href = "http://mandala.hagarsh.com";
+   window.location.href = "http://mandala.hagarsh.com/mobile";
  }, 2800);
 });
